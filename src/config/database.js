5cdd3d0fe789e4db16b0b78a4beb66ddef078bc5
Layek Miah <layekofficial63@gmail.com>
2025-11-10 21:20:19 +0600

    init project

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = db.connections[0].readyState === 1;
+    console.log("Database connect successfully");
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = connectDB;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors"); 
+const connectDB = require("./config/database");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "The wild oasis server is running" });
+});
+
+// Server setup
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
