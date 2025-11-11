aacb8be715f52b125bb617df91c67d6fb9c03138
Layek Miah <layekofficial63@gmail.com>
2025-11-11 06:38:38 +0600

    modify

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-  getBookings,
+//   getBookings,
   getBooking,
   createBooking,
   getBookedDates,
@@ -11,7 +11,7 @@ const {
 const router = express.Router();
 
 router.post("/", createBooking);
-router.get("/", getBookings); // <-- more specific first
+// router.get("/", getBookings); // <-- more specific first
 
 router.get("/:id/booked/dates", getBookedDates);
 router.patch("/:id/update", updateBooking);
