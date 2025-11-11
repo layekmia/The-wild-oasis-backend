966344cc5fa676d2c483197fb0df861d50857f8f
Layek Miah <layekofficial63@gmail.com>
2025-11-11 06:24:00 +0600

    debug

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const {
-  getBookings,
+//   getBookings,
   getBooking,
   createBooking,
-  getBookedDates,
-  updateBooking,
-  deleteBooking,
+//   getBookedDates,
+//   updateBooking,
+//   deleteBooking,
 } = require("../controller/booking.controller");
 
 const router = express.Router();
 
 router.post("/", createBooking);
-router.get("/", getBookings);
-router.get("/:id", getBooking);
-router.get("/:id/booked/dates", getBookedDates);
-router.patch("/:id/update", updateBooking);
-router.delete("/:id/delete", deleteBooking);
+// router.get("/", getBookings);
+// router.get("/:id", getBooking);
+// router.get("/:id/booked/dates", getBookedDates);
+// router.patch("/:id/update", updateBooking);
+// router.delete("/:id/delete", deleteBooking);
 
 module.exports = router;
